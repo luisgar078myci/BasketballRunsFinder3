3d50272ec0ca5b7aac629db8b4a5cb103818a2ca
luisgaribay <luis.garibay078@myci.csuci.edu>
2025-04-20 22:39:42 -0700

    Initial Commit

---
+++ BasketballRunsFinder3/BasketballRunsFinder3App.swift
@@ -0,0 +1,17 @@
+//
+//  BasketballRunsFinder3App.swift
+//  BasketballRunsFinder3
+//
+//  Created by Luis Garibay on 4/20/25.
+//
+
+import SwiftUI
+
+@main
+struct BasketballRunsFinder3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasketballRunsFinder3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BasketballRunsFinder3
+//
+//  Created by Luis Garibay on 4/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
